a89bbc7463b7740d8469bc70684b37c7db5a82e8
mcpenguin <marcus.chanwc@gmail.com>
2021-05-14 21:15:31 +0800

    Integrated React-Bootstrap; redesigned "Add Course" form

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@
 
 import Graph from "react-graph-vis";
 import React, { useState } from "react";
-import ReactDOM from "react-dom";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+// import ReactDOM from "react-dom";
 
 import { generateCourseNode, generateCourseEdge, parseMyClassEdgeData, parseMyClassNodeData } from './parse-data.js';
 import { colorLuminance } from './lighten-color.js';
@@ -27,8 +28,76 @@ const options = {
   }
 };
 
-// makeGraph: initialises the graph
-function MakeGraph() {
+class AddCourseForm extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      subjectCode: "",
+      catalogNumber: 0,
+      courseSeasons: [],
+      coursePrereqs: [],
+    }
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange = (e) => {
+    const target = e.target;
+    const value = ["subjectCode", "catalogNumber"].includes(target.value)
+      ? target.value
+      : target.value.split(";").filter(x => x);
+    this.setState({
+      [target.id]: value
+    });
+  }
+
+  handleSubmit = (e) => {
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <Form>
+        <Form.Group>
+          <Form.Label>Subject Code</Form.Label>
+          <Form.Control
+            type="text"
+            id="subjectCode"
+            onChange={(e) => this.setState({subjectCode: e.target.value})}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Catalog Number</Form.Label>
+          <Form.Control
+            type="text"
+            id="catalogNumber"
+            onChange={(e) => this.setState({catalogNumber: e.target.value})}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Seasons course offered</Form.Label>
+          <Form.Control
+            type="text"
+            id="courseSeasons"
+            onChange={(e) => this.setState({courseSeasons: e.target.value.split(";")})}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Course Prerequisites</Form.Label>
+          <Form.Control
+            type="text"
+            id="coursePrereqs"
+            onChange={(e) => this.setState({coursePrereqs: e.target.value.split(";")})}
+          />
+        </Form.Group>
+        <Button variant="primary" onClick={this.handleSubmit}>Add Course</Button>
+      </Form>
+    );
+  }
+}
+
+// main application
+function App() {
   // const createNode = (x, y) => {
   //   const color = randomColor();
   //   setState(({ graph: { nodes, edges }, counter, ...rest }) => {
@@ -50,6 +119,8 @@ function MakeGraph() {
   //     }
   //   });
   // }
+
+  var subjectCodeL, catalogNumberL, courseSeasonsL, coursePrereqsL;
 
   // dict object containing class data
   // (for now, its "test data")
@@ -114,6 +185,24 @@ function MakeGraph() {
     });
   }
 
+  // add course when "Add Course" button clicked
+  const addCourse = () => {
+    const subjectCode = subjectCodeL;
+    const catalogNumber = catalogNumberL;
+    const courseSeasons = courseSeasonsL.split(';').filter(x => x); // this gets rid of empty strings
+    const coursePrereqs = coursePrereqsL.split(';').filter(x => x);  // similar situation here
+
+    setState(({ graph: { nodes, edges }, ...rest }) => {
+      return {
+        graph: {
+          nodes: nodes.concat(generateCourseNode(subjectCode, catalogNumber, courseSeasons)),
+          edges: edges.concat(coursePrereqs.map((cp) => generateCourseEdge(subjectCode, catalogNumber, cp.split(" ")[0], cp.split(" ")[1]))),
+        },
+        ...rest
+      };
+    });
+  }
+
   // setting up the graph
   const [state, setState] = useState({
     graph: {
@@ -121,60 +210,51 @@ function MakeGraph() {
       edges: parseMyClassEdgeData(myClassDataDict),
     },
     events: {
-      // when selecting a node, "highlight" it 
+      // when selecting a node, "highlight" the edges connected to it
       select: ({ nodes, edges }) => {
         highlightEdgesConnectedToNode(nodes[0]);
       },
+      // when deselecting a node, "revert" the "selected" edges to "normal"
       deselectNode: ({ ...other }) => { revertEdgesToNormal() },
     }
   })
   const { graph, events } = state;
 
   return (
-    <div id="mynetwork">
-      <Graph graph={graph} options={options} events={events} />
-    </div>
-  );
-}
-
-// main application
-function App() {
-  return (
     <div id="App">
       <h1 id="heading">My Course Graph</h1>
 
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-lg-8">
-            <h4>Graph</h4>
-            {MakeGraph()}
-          </div>
-          <div class="col-lg-4">
-            <div class="container-fluid">
+      <Container fluid>
+        <Row>
+          <Col lg={8}>
+            <div id="mynetwork">
+              <Graph graph={graph} options={options} events={events} />
+            </div>
+          </Col>
+          <Col lg={4}>
+            <Container fluid>
               <h4>Add Course</h4>
-              <h5>Subject Code (e.g. MATH)</h5>
+              {/* <label htmlFor="subjectCode">Subject Code (e.g. MATH)</label>
               <input type="text" id="subjectCode" />
-              <h5>Catalog Number (e.g. 136)</h5>
+              <label htmlFor="catalogNumber">Catalog Number (e.g. 136)</label>
               <input type="text" id="catalogNumber" />
-              <h5>Seasons course offered (e.g. F;W;S)</h5>
-              <h6>Separate courses by semicolons</h6>
+              <label htmlFor="courseSeasons">Seasons course offered (e.g. F;W;S)</label>
               <input type="text" id="courseSeasons" />
-              <h5>Course Prerequisites (e.g. MATH 136; MATH 138)</h5>
-              <h6>Separate courses by semicolons</h6>
+              <label htmlFor="coursePrereqs">Course Prerequisites (e.g. MATH 136;MATH 138)</label>
               <input type="text" id="coursePrereqs" />
               <br />
-              <button id="addCourse">Add Course</button>
-            </div>
-            <div class="container-fluid">
-              <h5>Or, alternatively, import class data via a CSV:</h5>
+              <button id="addCourse" onClick={addCourse}>Add Course</button> */}
+              <AddCourseForm />
+            </Container>
+            <Container fluid>
+              <label>Or, alternatively, import class data via a CSV:</label>
               <h6>Each row should be in the form of</h6>
               <h6>subjectCode,catalogNumber,courseSeasons,coursePrereqs</h6>
               <input type="file" accept="csv" id="classDataFile" />
-              <button id="loadFile">Load File</button>
-            </div>
-          </div>
-        </div>
-      </div>
+            </Container>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
